4a3c3783cb16a52920399137ec68f9ea474512f0
Eirik Østensvik <eirikoo@gmail.com>
2020-08-24 20:51:05 +0200

    lagre

--- swiftUI_test/CardView.swift
+++ swiftUI_test/CardView.swift
@@ -8,14 +8,23 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CardView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        HStack {
+            VStack{
+            Text("Eirik Østensvik")
+            Text("Project Manager")
+            Text("Special Projects")
+            }
+            Image("Eirik")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        CardView()
     }
 }
